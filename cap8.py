bcb2d585c09e024b95f8c5ab867a725d6c787d99
w-hwg <60183186+Williamhwg@users.noreply.github.com>
2022-05-22 13:23:39 -0300

    Add files via upload

---
+++ cap8.py
@@ -0,0 +1,44 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+# %%
+table18 = pd.read_csv('table18.txt', index_col = 'i', sep=' ', dtype={'xi': 'float', 'yi': 'float'})
+table19 = pd.read_csv('table19.txt', index_col = 'i', sep=' ', dtype={'Ni': 'float', 'ti': 'float', 'di': 'float', 'li': 'float', 'hi': 'float','bi': 'float', 'gi': 'float', 'ei': 'float'})
+table19.columns = ['Ni','ti','di','li','hi','bi','gi','ei']
+# %%
+# Constants
+Tc = 514.71 # K
+rho_c = 5.93 # mol/dm³
+R = 8.31446262 #J/(mol.K)
+# %%
+T = np.linspace(100,700,100)
+rho = np.linspace(1, 22, 100)
+tau = Tc/T
+delta = rho/rho_c
+# %%
+xdelta, xtau = np.meshgrid(delta,tau)
+# %%
+def alpha_0(delta, tau): # J/mol
+    sum_alpha0 = np.cumsum([table18['xi'][i]*(1-np.log(1-np.exp(-table18['yi'][i]*tau))) for i in range(4,8)], axis=1)[-1]
+    other_terms0 = np.log(delta) + table18['xi'][1]*np.log(tau) + table18['xi'][2] + table18['xi'][3]*tau
+    return (other_terms0 + sum_alpha0)*T*R
+
+def alpha_r(delta, tau):
+    sum_r1 = np.cumsum([table19['Ni'][i]*delta**(table19['di'][i])*tau**(table19['ti'][i]) for i in range(1,7)], axis=1)[-1]
+    sum_r2 = np.cumsum([table19['Ni'][i]*delta**(table19['di'][i])*tau**(table19['ti'][i])*np.exp(-delta**(table19['li'][i])) for i in range(7,17)], axis=1)[-1]
+    sum_r3 = np.cumsum([table19['Ni'][i]*delta**(table19['di'][i])*tau**(table19['ti'][i])*np.exp((-table19['hi'][i]*(delta-table19['ei'][i])**2-table19['bi'][i]*(tau-table19['gi'][i])**2)) for i in range(17,26)], axis=1)[-1]
+    return sum_r1 + sum_r2 + sum_r3
+
+def helm(delta, tau):
+    return alpha_0(delta, tau) + alpha_r(delta,tau)
+#%%
+np.cumsum([[1, 2],[3,4]], axis=1)
+# %%
+plt.contourf(xdelta*rho_c, Tc/xtau, helm(xdelta,xtau), levels=40, cmap='RdBu')
+plt.xlabel('Density (mol/dm³)')
+plt.ylabel('Temperature (K)')
+plt.colorbar()
+# %%
